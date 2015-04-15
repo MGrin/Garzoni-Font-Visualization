6214cb8f78b394d30aeebba7adbfdc75f92acfa0
MGrin <mr6r1n@gmail.com>
2015-04-15 16:57:58 +0200

    working

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/client/jade');
+app.use(express.static(__dirname + '/client'));
 
-app.get('/', function(req, res) {
-  res.send('Hello Garzoni!');
+app.get('/', function (req, res) {
+  res.render('index.jade');
+});
+
+app.get('/data', function (req, res) {
+  res.sendFile(__dirname + '/client/data/garzoni.csv');
 });
 
 app.listen(app.get('port'), function() {
